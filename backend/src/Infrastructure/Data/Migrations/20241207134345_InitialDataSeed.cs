adb69c898db0edc14436998f9daf585ddc5e00b0
rewqla <desares526@gmail.com>
2024-12-07 18:42:11 +0200

    Added Users seed with password

--- backend/src/Infrastructure/Data/Migrations/20241207134345_InitialDataSeed.cs
+++ backend/src/Infrastructure/Data/Migrations/20241207134345_InitialDataSeed.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -7,22 +8,30 @@ namespace Infrastructure.Data.Migrations
     /// <inheritdoc />
     public partial class InitialDataSeed : Migration
     {
+        string HashPassword(string password)
+        {
+            using (var sha512 = SHA512.Create())
+            {
+                byte[] salt = RandomNumberGenerator.GetBytes(16);
+                byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, 500000, HashAlgorithmName.SHA512, 32);
+
+                return $"{Convert.ToHexString(hash)}-{Convert.ToHexString(salt)}";
+            }
+        }
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql($"INSERT INTO \"Roles\" (\"Id\", \"Name\") VALUES ('{Guid.NewGuid()}','Admin')");
-            migrationBuilder.Sql($"INSERT INTO \"Roles\" (\"Id\", \"Name\") VALUES ('{Guid.NewGuid()}','Manager')");
-            migrationBuilder.Sql($"INSERT INTO \"Roles\" (\"Id\", \"Name\") VALUES ('{Guid.NewGuid()}','Worker')");
+            var adminRoleId = Guid.NewGuid();
+            var managerRoleId = Guid.NewGuid();
+            var workerRoleId = Guid.NewGuid();
             
-            // migrationBuilder.InsertData(
-            //     table: "Roles",
-            //     columns: new[] { "Id", "Name" },
-            //     values: new object[,]
-            //     {
-            //         { Guid.NewGuid(), "Admin" },
-            //         { Guid.NewGuid(), "Manager" },
-            //         { Guid.NewGuid(), "User" }
-            //     });
+            migrationBuilder.Sql($"INSERT INTO \"Roles\" (\"Id\", \"Name\") VALUES ('{adminRoleId}','Admin')");
+            migrationBuilder.Sql($"INSERT INTO \"Roles\" (\"Id\", \"Name\") VALUES ('{managerRoleId}','Manager')");
+            migrationBuilder.Sql($"INSERT INTO \"Roles\" (\"Id\", \"Name\") VALUES ('{workerRoleId}','Worker')");
+            
+            migrationBuilder.Sql($"INSERT INTO \"Users\" (\"Id\", \"Name\", \"Phone\", \"Email\", \"PasswordHash\", \"RoleId\") VALUES ('{Guid.NewGuid()}', 'Admin User', '1234567890', 'admin@example.com', '{HashPassword("123456")}', '{adminRoleId}')");
+            migrationBuilder.Sql($"INSERT INTO \"Users\" (\"Id\", \"Name\", \"Phone\", \"Email\", \"PasswordHash\", \"RoleId\") VALUES ('{Guid.NewGuid()}', 'Manager User', '0987654321', 'manager@example.com', '{HashPassword("123456")}', '{managerRoleId}')");
+            migrationBuilder.Sql($"INSERT INTO \"Users\" (\"Id\", \"Name\", \"Phone\", \"Email\", \"PasswordHash\", \"RoleId\") VALUES ('{Guid.NewGuid()}', 'Worker User', '1112223333', 'worker@example.com', '{HashPassword("123456")}', '{workerRoleId}')");
         }
 
         /// <inheritdoc />
