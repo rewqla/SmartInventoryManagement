f53c780f2515d11fa5859a409b675d5436c2e438
rewqla <desares526@gmail.com>
2024-12-08 10:53:30 +0200

    Added warehouse seed

--- backend/src/Infrastructure/Data/Migrations/20241207134345_InitialDataSeed.cs
+++ backend/src/Infrastructure/Data/Migrations/20241207134345_InitialDataSeed.cs
@@ -23,6 +23,8 @@ namespace Infrastructure.Data.Migrations
         {
             SeedRoles(migrationBuilder);
             SeedUsers(migrationBuilder);
+            SeedCategories(migrationBuilder);
+            SeedWarehouses(migrationBuilder);
         }
 
         private Guid _adminRoleId;
@@ -46,6 +48,37 @@ namespace Infrastructure.Data.Migrations
             migrationBuilder.Sql($"INSERT INTO \"Users\" (\"Id\", \"Name\", \"Phone\", \"Email\", \"PasswordHash\", \"RoleId\") VALUES ('{Guid.NewGuid()}', 'Manager User', '0987654321', 'manager@example.com', '{HashPassword("123456")}', '{_managerRoleId}')");
             migrationBuilder.Sql($"INSERT INTO \"Users\" (\"Id\", \"Name\", \"Phone\", \"Email\", \"PasswordHash\", \"RoleId\") VALUES ('{Guid.NewGuid()}', 'Worker User', '1112223333', 'worker@example.com', '{HashPassword("123456")}', '{_workerRoleId}')");
         }
+        
+        private Guid _electronicsCategoryId;
+        private Guid _furnitureCategoryId;
+        private Guid _clothingCategoryId;
+        private Guid _sportsCategoryId;
+        private Guid _beautyCategoryId;
+        private void SeedCategories(MigrationBuilder migrationBuilder)
+        {
+            _electronicsCategoryId = Guid.NewGuid();
+            _furnitureCategoryId = Guid.NewGuid();
+            _clothingCategoryId = Guid.NewGuid();
+            _sportsCategoryId = Guid.NewGuid();
+            _beautyCategoryId = Guid.NewGuid();
+
+            migrationBuilder.Sql($"INSERT INTO \"Categories\" (\"Id\", \"Name\") VALUES ('{_electronicsCategoryId}', 'Electronics')");
+            migrationBuilder.Sql($"INSERT INTO \"Categories\" (\"Id\", \"Name\") VALUES ('{_furnitureCategoryId}', 'Furniture')");
+            migrationBuilder.Sql($"INSERT INTO \"Categories\" (\"Id\", \"Name\") VALUES ('{_clothingCategoryId}', 'Clothing')");
+            migrationBuilder.Sql($"INSERT INTO \"Categories\" (\"Id\", \"Name\") VALUES ('{_sportsCategoryId}', 'Sports')");
+            migrationBuilder.Sql($"INSERT INTO \"Categories\" (\"Id\", \"Name\") VALUES ('{_beautyCategoryId}', 'Beauty')");
+        }
+        private Guid _warehouse1Id;
+        private Guid _warehouse2Id;
+        private void SeedWarehouses(MigrationBuilder migrationBuilder)
+        {
+            _warehouse1Id = Guid.NewGuid();
+            _warehouse2Id = Guid.NewGuid();
+
+            migrationBuilder.Sql($"INSERT INTO \"Warehouses\" (\"Id\", \"Name\", \"Location\") VALUES ('{_warehouse1Id}', 'Central Warehouse','Rivne')");
+            migrationBuilder.Sql($"INSERT INTO \"Warehouses\" (\"Id\", \"Name\", \"Location\") VALUES ('{_warehouse2Id}', 'Secondary Warehouse', 'Zhytomyr')");
+        }
+        
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
