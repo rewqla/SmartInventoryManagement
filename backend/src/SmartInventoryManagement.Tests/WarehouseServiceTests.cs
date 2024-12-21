9364fd2a2164f1845c55dc59c874d40b8bb971e5
rewqla <desares526@gmail.com>
2024-12-21 13:50:48 +0200

    Added logging test

--- backend/src/SmartInventoryManagement.Tests/WarehouseServiceTests.cs
+++ backend/src/SmartInventoryManagement.Tests/WarehouseServiceTests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Infrastructure.Entities;
 using Infrastructure.Interfaces.Repositories.Warehouse;
+using Microsoft.Extensions.Logging;
 using Moq;
 
 namespace SmartInventoryManagement.Tests;
@@ -9,12 +10,14 @@ namespace SmartInventoryManagement.Tests;
 public class WarehouseServiceTests
 {
     private readonly WarehouseService _warehouseService;
+    private readonly Mock<ILogger<WarehouseService>> _logger;
     private readonly Mock<IWarehouseRepository> _warehouseRepository;
 
     public WarehouseServiceTests()
     {
         _warehouseRepository = new Mock<IWarehouseRepository>();
-        _warehouseService = new WarehouseService(_warehouseRepository.Object);
+        _logger = new Mock<ILogger<WarehouseService>>();
+        _warehouseService = new WarehouseService(_warehouseRepository.Object, _logger.Object);
     }
 
     [Fact]
@@ -55,5 +58,27 @@ public class WarehouseServiceTests
         // Assert
         result.Should().BeEquivalentTo(expectedWarehouses, options => options
             .Excluding(warehouse => warehouse.Inventories));
+    }
+    
+    [Fact]
+    public async Task GetWarehousesAsync_ShouldLogMessages_WhenInvoked()
+    {
+        // Arrange
+        _warehouseRepository.Setup(r => r.GetAllAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Enumerable.Empty<Warehouse>())
+            .Verifiable();
+
+        // Act
+        var result = await _warehouseService.GetWarehousesAsync();
+
+        // Assert
+        _logger.Verify(
+            x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((o, t) => string.Equals("Retrieve all warehouses", o.ToString(), StringComparison.InvariantCultureIgnoreCase)),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
     }
 }
