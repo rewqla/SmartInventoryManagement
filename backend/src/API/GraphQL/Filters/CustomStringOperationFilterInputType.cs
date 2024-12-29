face0907090432c1427d888dc9a37819f388fff1
rewqla <desares526@gmail.com>
2024-12-29 17:04:13 +0200

    Restricted allowed filtering operations

---
+++ backend/src/API/GraphQL/Filters/CustomStringOperationFilterInputType.cs
@@ -0,0 +1,15 @@
+﻿using HotChocolate.Data.Filters;
+
+namespace API.GraphQL.Filters;
+
+internal class CustomStringOperationFilterInputType : StringOperationFilterInputType
+{
+    protected override void Configure(IFilterInputTypeDescriptor descriptor)
+    {
+        descriptor.Operation(DefaultFilterOperations.Equals).Type<StringType>();
+        descriptor.Operation(DefaultFilterOperations.NotEquals).Type<StringType>();
+        descriptor.Operation(DefaultFilterOperations.StartsWith).Type<StringType>();
+        descriptor.Operation(DefaultFilterOperations.Contains).Type<StringType>();
+        descriptor.Operation(DefaultFilterOperations.In).Type<StringType>();
+    }
+}
--- backend/src/API/GraphQL/Filters/WarehouseFilterType.cs
+++ backend/src/API/GraphQL/Filters/WarehouseFilterType.cs
@@ -10,7 +10,9 @@ public class WarehouseFilterType : FilterInputType<WarehouseDTO>
     protected override void Configure(IFilterInputTypeDescriptor<WarehouseDTO> descriptor)
     {
         descriptor.Ignore(c => c.Id);
-
+        descriptor.Field(f => f.Name).Type<CustomStringOperationFilterInputType>();
+        descriptor.Field(f => f.Location).Type<CustomStringOperationFilterInputType>();
+        
         base.Configure(descriptor);
     }
 }
