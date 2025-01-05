f4f6e556a16da2e164fe392023872f73fc49de3a
rewqla <desares526@gmail.com>
2025-01-05 11:13:52 +0200

    Added missing #todos

--- backend/src/API/Endpoints/Warehouse/WarehouseEndpointExtension.cs
+++ backend/src/API/Endpoints/Warehouse/WarehouseEndpointExtension.cs
@@ -5,7 +5,7 @@ public static class WarehouseEndpointExtension
     public static IEndpointRouteBuilder MapWarehouseEndpoints(this IEndpointRouteBuilder app)
     {
         app.MapGetWarehouses();
-        
+        // #todo create add, update, delete, get by id warehouse endpoints
         return app;
     }
 }
