65488d6297105e705d281dde9b07074017e4ac31
rewqla <desares526@gmail.com>
2025-01-28 19:01:02 +0200

    Added product query

---
+++ backend/src/API/GraphQL/Queries/ProductQueries.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Data;
+using Infrastructure.Entities;
+
+namespace API.GraphQL.Queries;
+
+[QueryType]
+public sealed class ProductQueries
+{
+    public IQueryable<Product> GetProducts([Service] InventoryContext context)
+    {
+        return context.Products.Select(p => new Product
+        {
+            Id = p.Id,
+            Name = p.Name,
+            Description = p.Description,
+            UnitPrice = p.UnitPrice,
+            SKU = p.SKU
+        });
+    }
+}
