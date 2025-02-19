51c975209b74d73d98255f4da7ab501ef3826b82
rewqla <desares526@gmail.com>
2025-02-19 19:28:15 +0200

    Moved product service to use product repository

--- backend/src/Application/Services/Product/ProductService.cs
+++ backend/src/Application/Services/Product/ProductService.cs
@@ -2,23 +2,24 @@
 using Application.Interfaces.Services.Product;
 using Application.Interfaces.Services.Report;
 using Infrastructure.Data;
+using Infrastructure.Interfaces.Repositories;
 
 namespace Application.Services.Product;
 
 public class ProductService : IProductService
 {
     private readonly IReportService<Infrastructure.Entities.Product> _productReportService;
-    private readonly InventoryContext _context;
+    private readonly IProductRepository _productRepository;
 
-    public ProductService(IReportService<Infrastructure.Entities.Product> productReportService, InventoryContext context)
+    public ProductService(IReportService<Infrastructure.Entities.Product> productReportService, IProductRepository productRepository)
     {
         _productReportService = productReportService;
-        _context = context;
+        _productRepository = productRepository;
     }
 
     public async Task<Result<byte[]>> GenerateProductReportAsync(CancellationToken cancellationToken = default)
     {
-        var products = _context.Products.ToList();
+        var products = await _productRepository.GetAllAsync(cancellationToken);
         var report = _productReportService.GenerateReport(products);
 
         return Result<byte[]>.Success(report);
