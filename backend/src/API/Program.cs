7be9b708300f6644edab4b7190917ddbb0289f92
rewqla <desares526@gmail.com>
2025-02-22 12:46:07 +0200

    Added check if to apply migrations

--- backend/src/API/Program.cs
+++ backend/src/API/Program.cs
@@ -15,11 +15,18 @@ builder.ConfigureGraphQL();
 
 // todo: add serilog and magnify logging
 // todo: add request middleware for logging urls and methods
+// todo: add appsettings.Production.json 
 
 var app = builder.Build();
 
 app.ConfigureMiddlewares();
 app.MapEndpoints();
-app.ApplyMigrationsAndGenerateScripts();
+
+var applyMigrations = builder.Configuration.GetValue<bool>("ApplyMigrations");
+
+if (applyMigrations)
+{
+    app.ApplyMigrationsAndGenerateScripts();
+}
 
 app.RunWithGraphQLCommands(args);
