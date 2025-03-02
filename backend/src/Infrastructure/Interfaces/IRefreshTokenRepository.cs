148957e38ca2e8839dfce604d023296f275eb9da
rewqla <desares526@gmail.com>
2025-03-02 13:19:54 +0200

    Added refresh token repository

---
+++ backend/src/Infrastructure/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Entities;
+
+namespace Infrastructure.Interfaces;
+
+public interface  IRefreshTokenRepository
+{
+    Task SaveRefreshTokenAsync(RefreshToken refreshToken);
+    Task RemoveOldRefreshTokensAsync(Guid userId);
+    Task<RefreshToken?> GetRefreshTokenAsync(string token);
+}
---
+++ backend/src/Infrastructure/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,37 @@
+﻿using Infrastructure.Data;
+using Infrastructure.Entities;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class RefreshTokenRepository: IRefreshTokenRepository
+{
+    private readonly InventoryContext _context;
+
+    public RefreshTokenRepository(InventoryContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SaveRefreshTokenAsync(RefreshToken refreshToken)
+    {
+        await _context.RefreshTokens.AddAsync(refreshToken);
+        
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task RemoveOldRefreshTokensAsync(Guid userId)
+    {
+        var oldTokens = _context.RefreshTokens.Where(rt => rt.UserId == userId);
+        _context.RefreshTokens.RemoveRange(oldTokens);
+        
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<RefreshToken?> GetRefreshTokenAsync(string token)
+    {
+        return await _context.RefreshTokens
+            .FirstOrDefaultAsync(rt => rt.Token == token);
+    }
+}
