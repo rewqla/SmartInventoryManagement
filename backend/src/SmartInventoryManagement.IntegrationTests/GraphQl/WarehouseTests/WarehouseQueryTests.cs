086f32d8bfdc3b3729b63bf3c5b9bf980ce2b66c
rewqla <desares526@gmail.com>
2025-03-15 15:47:11 +0200

    Added sorting for tests to retrieve the same results

--- backend/src/SmartInventoryManagement.IntegrationTests/GraphQl/WarehouseTests/WarehouseQueryTests.cs
+++ backend/src/SmartInventoryManagement.IntegrationTests/GraphQl/WarehouseTests/WarehouseQueryTests.cs
@@ -44,6 +44,9 @@ public class WarehouseQueryTests
                 { location: { eq: ""Amsterdam"" } }
               ]
             }
+            order: [ {
+               name: ASC
+            }]
           ) {
             items {
               name
