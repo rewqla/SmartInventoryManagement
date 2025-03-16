dd4ad272230451199083c1309c3f12d5b499d6ed
rewqla <desares526@gmail.com>
2025-03-16 09:40:05 +0200

    Added UserAlreadyExists error code

--- backend/src/Application/Exceptions/AuthenticationErrors.cs
+++ backend/src/Application/Exceptions/AuthenticationErrors.cs
@@ -6,4 +6,7 @@ public static class AuthenticationErrors
 {
     public static Error InvalidCredentials() => new Error(
         "Authentication.InvalidCredentials", "Incorrect password");
+    
+    public static Error UserAlreadyExists() =>
+        new Error("Authentication.UserAlreadyExists", "A user with this email ot phone already exists.");
 }
