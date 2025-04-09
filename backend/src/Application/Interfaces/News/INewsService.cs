b033651f8061c7c21339b3c7340792b42b581cd2
rewqla <desares526@gmail.com>
2025-04-09 19:08:30 +0300

    Added news service with refit client implementation

---
+++ backend/src/Application/Interfaces/News/INewsService.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Interfaces.News;
+
+public interface INewsService
+{
+    Task<List<string>> GetTopHeadlinesTitlesAsync(string query, CancellationToken cancellationToken = default);
+}
---
+++ backend/src/Application/Services/News/NewsService.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces.News;
+using Infrastructure.Interfaces;
+
+namespace Application.Services.News;
+
+public class NewsService : INewsService
+{
+    private readonly INewsApi _newsApi;
+
+    public NewsService(INewsApi newsApi)
+    {
+        _newsApi = newsApi;
+    }
+
+    public async Task<List<string>> GetTopHeadlinesTitlesAsync(string query, CancellationToken cancellationToken = default)
+    {
+        var articles = await _newsApi.GetTopHeadlinesAsync(query, cancellationToken);
+        return articles.Articles.Select(a => a.Title).ToList();
+    }
+}
