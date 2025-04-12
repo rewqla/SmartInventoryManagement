bb676e28402cd8e269427645ad1bf5052de47186
rewqla <desares526@gmail.com>
2025-04-12 18:47:52 +0300

    Added ValidationResultExtensions for better retrieving error (validation) details

---
+++ backend/src/Application/Validation/ValidationResultExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common;
+
+namespace Application.Validation;
+
+public static class ValidationResultExtensions
+{
+    public static List<ErrorDetail> ToErrorDetails(this FluentValidation.Results.ValidationResult validationResult)
+    {
+        return validationResult.Errors.Select(error => new ErrorDetail
+        {
+            PropertyName = error.PropertyName,
+            ErrorMessage = error.ErrorMessage
+        }).ToList();
+    }
+}
