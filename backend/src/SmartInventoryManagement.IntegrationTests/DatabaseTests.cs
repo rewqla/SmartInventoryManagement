c61e5e55ccb79cfb989b7fc89ff8855053f99d85
rewqla <desares526@gmail.com>
2025-04-19 17:37:55 +0300

    Added a test to ensure the database is migrated

---
+++ backend/src/SmartInventoryManagement.IntegrationTests/DatabaseTests.cs
@@ -0,0 +1,30 @@
+﻿using SmartInventoryManagement.IntegrationTests.Api.Tests;
+
+namespace SmartInventoryManagement.IntegrationTests;
+
+public class DatabaseTests : IClassFixture<CustomWebApplicationFactory>, IAsyncLifetime
+{
+    private readonly CustomWebApplicationFactory _applicationFactory;
+
+    public DatabaseTests(CustomWebApplicationFactory applicationFactory)
+    {
+        _applicationFactory = applicationFactory;
+    }
+
+    [Fact]
+    public async Task DatabaseTest_ShouldAllMigrationBeInstalled()
+    {
+        // Arrange
+        using var scope = _applicationFactory.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<InventoryContext>();
+
+        var appliedMigrations = await context.Database.GetAppliedMigrationsAsync();
+        var availableMigrations = context.Database.GetMigrations();
+
+        // Assert
+        appliedMigrations.Should().BeEquivalentTo(availableMigrations);
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+    public Task DisposeAsync() => Task.CompletedTask;
+}
