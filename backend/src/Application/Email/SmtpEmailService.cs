4fab4dc868fbe1849b556ebdce28af94c28a4996
rewqla <desares526@gmail.com>
2025-04-22 12:33:53 +0300

    Added email interface and SmtpEmailService implementation

---
+++ backend/src/Application/Email/SmtpEmailService.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces;
+using FluentEmail.Core;
+
+namespace Application.Email;
+
+public class SmtpEmailService : IEmailService
+{
+    private readonly IFluentEmail _fluentEmail;
+
+    public SmtpEmailService(IFluentEmail fluentEmail)
+    {
+        _fluentEmail = fluentEmail;
+    }
+
+    public async Task SendEmailAsync(string to, string subject, string body)
+    {
+        var result = await _fluentEmail.To(to)
+            .Subject(subject)
+            .Body(body)
+            .SendAsync();
+    }
+}
---
+++ backend/src/Application/Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Interfaces;
+
+public interface IEmailService
+{
+    //todo: add email metadata https://www.youtube.com/watch?v=y4Q-Hp7HY_U
+    //todo: send by template
+    //todo: handle errors, as smtp is not the main thing in the application
+    Task SendEmailAsync(string to, string subject, string body);
+}
