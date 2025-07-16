b93fdd2de589d31b207515d134a618b47746fa67
rewqla <desares526@gmail.com>
2025-07-16 13:02:16 +0300

    Moved SignUpEndpoint to fast endpoints

--- backend/src/API/Endpoints/Auth/SignUpEndpoint.cs
+++ backend/src/API/Endpoints/Auth/SignUpEndpoint.cs
@@ -3,65 +3,134 @@ using API.Extensions;
 using Application.DTO.Authentication;
 using Application.Exceptions;
 using Application.Interfaces.Authentication;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Endpoints.Auth;
 
-public static class SignUpEndpoint
+// public static class SignUpEndpoint
+// {
+//     private const string Name = "SignUp";
+//
+//     public static IEndpointRouteBuilder MapSignUp(this IEndpointRouteBuilder app)
+//     {
+//         app.MapPost(AuthEndpoints.SignUp,
+//                 async ([FromServices] IAuthenticationService authenticationService, [Microsoft.AspNetCore.Mvc.FromBody] SignUpDTO request,
+//                     CancellationToken cancellationToken) =>
+//                 {
+//                     var result = await authenticationService.SignUpAsync(request);
+//
+//                     return result.Match(
+//                         onSuccess: _ => Results.NoContent(),
+//                         onFailure: error =>
+//                         {
+//                             return error.Code switch
+//                             {
+//                                 "Authentication.EmailAlreadyExists" => Results
+//                                     .Conflict(new
+//                                     {
+//                                         type = "https://httpstatuses.com/409",
+//                                         title = error.Code,
+//                                         detail = error.Description,
+//                                         statusCode = StatusCodes.Status409Conflict
+//                                     }),
+//                                 "Authentication.PhoneAlreadyExists" => Results
+//                                     .Conflict(new
+//                                     {
+//                                         type = "https://httpstatuses.com/409",
+//                                         title = error.Code,
+//                                         detail = error.Description,
+//                                         statusCode = StatusCodes.Status409Conflict
+//                                     }),
+//                                 "Common.NotFound.role" => Results.NotFound(new
+//                                 {
+//                                     type = "https://httpstatuses.com/404",
+//                                     title = error.Code,
+//                                     detail = error.Description,
+//                                     statusCode = StatusCodes.Status404NotFound
+//                                 }),
+//                                 "SignUpDTO.ValidationError" => Results.Problem(type: "Bad Request",
+//                                     title: error.Code,
+//                                     detail: error.Description,
+//                                     statusCode: StatusCodes.Status400BadRequest,
+//                                     extensions: new Dictionary<string, object?>
+//                                     {
+//                                         { "errors", error.Errors }
+//                                     })
+//                             };
+//                         });
+//                 })
+//             .WithName(Name)
+//             .WithTags(EndpointTags.Auth);
+//
+//         return app;
+//     }
+// }
+
+//todo: move to RERP structure (request, response, validation)
+internal sealed class
+    SignUpEndpoint : Endpoint<SignUpDTO, Results<Ok<AuthenticationDTO>, NotFound, BadRequest>>
 {
-    private const string Name = "SignUp";
+    public IAuthenticationService authenticationService { get; set; }
 
-    public static IEndpointRouteBuilder MapSignUp(this IEndpointRouteBuilder app)
+    public override void Configure()
     {
-        app.MapPost(AuthEndpoints.SignUp,
-                async ([FromServices] IAuthenticationService authenticationService, [FromBody] SignUpDTO request,
-                    CancellationToken cancellationToken) =>
+        Post(AuthEndpoints.SignUp);
+        Description(x => x.WithTags(EndpointTags.Auth));
+        AllowAnonymous();
+    }
+
+    //todo: think about refactoring of returning types
+    public override async Task HandleAsync(SignUpDTO request, CancellationToken cancellationToken)
+    {
+        var result = await authenticationService.SignUpAsync(request);
+
+        if (result.IsSuccess)
+        {
+            await SendResultAsync(TypedResults.NoContent());
+            return;
+        }
+
+        //todo: update check not by string, but as object
+        var error = result.Error;
+
+        switch (error.Code)
+        {
+            case "Authentication.EmailAlreadyExists":
+            case "Authentication.PhoneAlreadyExists":
+                await SendResultAsync(TypedResults.Conflict(new
                 {
-                    var result = await authenticationService.SignUpAsync(request);
+                    type = "https://httpstatuses.com/409",
+                    title = error.Code,
+                    detail = error.Description,
+                    statusCode = StatusCodes.Status409Conflict
+                }));
+                break;
 
-                    return result.Match(
-                        onSuccess: _ => Results.NoContent(),
-                        onFailure: error =>
-                        {
-                            return error.Code switch
-                            {
-                                "Authentication.EmailAlreadyExists" => Results
-                                    .Conflict(new
-                                    {
-                                        type = "https://httpstatuses.com/409",
-                                        title = error.Code,
-                                        detail = error.Description,
-                                        statusCode = StatusCodes.Status409Conflict
-                                    }),
-                                "Authentication.PhoneAlreadyExists" => Results
-                                    .Conflict(new
-                                    {
-                                        type = "https://httpstatuses.com/409",
-                                        title = error.Code,
-                                        detail = error.Description,
-                                        statusCode = StatusCodes.Status409Conflict
-                                    }),
-                                "Common.NotFound.role" => Results.NotFound(new
-                                {
-                                    type = "https://httpstatuses.com/404",
-                                    title = error.Code,
-                                    detail = error.Description,
-                                    statusCode = StatusCodes.Status404NotFound
-                                }),
-                                "SignUpDTO.ValidationError" => Results.Problem(type: "Bad Request",
-                                    title: error.Code,
-                                    detail: error.Description,
-                                    statusCode: StatusCodes.Status400BadRequest,
-                                    extensions: new Dictionary<string, object?>
-                                    {
-                                        { "errors", error.Errors }
-                                    })
-                            };
-                        });
-                })
-            .WithName(Name)
-            .WithTags(EndpointTags.Auth);
+            case "Common.NotFound.role":
+                await SendResultAsync(TypedResults.Conflict(new
+                {
+                    type = "https://httpstatuses.com/404",
+                    title = error.Code,
+                    detail = error.Description,
+                    statusCode = StatusCodes.Status404NotFound
+                }));
+                break;
 
-        return app;
+            case "SignUpDTO.ValidationError":
+                await SendResultAsync(TypedResults.BadRequest(new
+                {
+                    type = "https://httpstatuses.com/400",
+                    title = error.Code,
+                    detail = error.Description,
+                    statusCode = StatusCodes.Status400BadRequest,
+                    extensions = new Dictionary<string, object?>
+                    {
+                        { "errors", error.Errors }
+                    }
+                }));
+                break;
+        }
     }
 }
