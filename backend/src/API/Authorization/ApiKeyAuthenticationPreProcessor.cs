ec2d9aff4b164ddaf828a3bf1809ebfbb3b0df50
rewqla <desares526@gmail.com>
2025-07-19 16:55:18 +0300

    Moved SignInEndpoint to fast endpoints

---
+++ backend/src/API/Authorization/ApiKeyAuthenticationPreProcessor.cs
@@ -0,0 +1,28 @@
+﻿using FastEndpoints;
+
+namespace API.Authorization;
+
+public sealed class ApiKeyAuthenticationPreProcessor : IPreProcessor<EmptyRequest>
+{
+    private const string ApiKeyHeaderName = "ApiKey";
+    private readonly IConfiguration _configuration;
+
+    public ApiKeyAuthenticationPreProcessor(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public Task PreProcessAsync(IPreProcessorContext<EmptyRequest> context, CancellationToken ct)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        var apiKey = context.HttpContext.Request.Headers[ApiKeyHeaderName].FirstOrDefault();
+
+        if (string.IsNullOrWhiteSpace(apiKey) || apiKey != _configuration.GetValue<string>("ApiKey"))
+        {
+            return context.HttpContext.Response.SendUnauthorizedAsync(ct);
+        }
+
+        return Task.CompletedTask;
+    }
+}
